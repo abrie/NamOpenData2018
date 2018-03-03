76140fc5577ab64c7dd5452cd7e759b7f564d20a
abrie <abrie@example.com>
2018-03-03 10:02:57 -0500

    Moving toward processing raw CSV.

--- logic.js
+++ logic.js
@@ -1,8 +1,9 @@
-const input = {
+const state = {
   hour: undefined,
   day: undefined,
   month: undefined,
   gender: undefined,
+  age: undefined,
   driving: undefined,
   vehicle_type: undefined,
 }
@@ -18,17 +19,27 @@ const crashTargetSelector = document.getElementById("crash-target-selector");
 crashTargetSelector.addEventListener("change", (evt) => console.log(evt.target.value));
 
 const hourSelector = document.getElementById("hour-selector");
-hourSelector.addEventListener("input", (evt) => input.hour = evt.target.value);
+hourSelector.addEventListener("input", (evt) => state.hour = evt.target.value);
 
 const daySelector = document.getElementById("day-selector");
-daySelector.addEventListener("input", (evt) => input.day = evt.target.value);
+daySelector.addEventListener("input", (evt) => state.day = evt.target.value);
 
 const monthSelector = document.getElementById("month-selector");
-monthSelector.addEventListener("input", (evt) => input.month = evt.target.value);
+monthSelector.addEventListener("input", (evt) => state.month = evt.target.value);
 
 populateVehicleTypeSelector( document.getElementById("vehicle-selector") );
+populateAgeSelector( document.getElementById("age-selector") );
 
 function populateVehicleTypeSelector(element) {
   vehicle_types.forEach( function(str) { element.appendChild(createOption(str)) } );
+}
+
+function populateAgeSelector( el ) {
+  getAgeGroups().forEach( (ageGroup) => {
+    const op = document.createElement("option");
+    op.setAttribute("value", ageGroup);
+    op.innerHTML = ageGroup;
+    el.appendChild(op);
+  });
 }
 
